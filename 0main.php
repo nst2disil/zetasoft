f7919f0a3c9f8d868cf93a547927ab2f222f6353
Kirilll <k.kovalenko@ctrl2go.com>
2023-01-30 12:29:55 +0700

    + Главная: 3 и 4 блоки

--- 0main.php
+++ 0main.php
@@ -60,6 +60,51 @@
       </div>
     </div>
   </section>
-  
+  <div class="wrapper">
+    <section class="section section--ill2">
+      <div class="section__info">
+        <h1 class="section__title">Наши компетенции</h1>
+        <p class="section__text">
+          Как ведутся, оформляются, проектируются, реализуются задачи и описывается результат. Управляйте вашими задачами - определяйте приоритеты, смотрите открытые задачи и их выполнение.
+        </p>
+        <div class="bricks">
+          <div class="bricks__item">Автобизнес</div>
+          <div class="bricks__item">Вендинг</div>
+          <div class="bricks__item bricks__item--fs40">Другие отраслевые компетенции</div>
+        </div>
+      </div>
+    </section>
+  </div>
+  <section class="section section--dark section--ill3">
+    <div class="wrapper">
+      <div class="section__title">Что входит в наши услуги</div>
+      <div class="section__rotated-title">Люди</div>
+      <div class="bricks">
+        <div class="bricks__item">Аналитики</div>
+        <div class="bricks__item">Мастера по программированию</div>
+        <div class="bricks__item">Архитекторы</div>
+        <div class="bricks__item">Специалисты поддержки</div>
+      </div>
+      <div class="section__rotated-title">Технологии</div>
+      <div class="bricks">
+        <div class="bricks__item">Канбан</div>
+        <div class="bricks__item">Smart</div>
+        <div class="bricks__item">git</div>
+        <div class="bricks__item">База знаний</div>
+        <div class="bricks__item">Контроль качества кода</div>
+      </div>
+      <div class="image-block">
+        <div class="image-block__text">Опыт более <span class="image-block__red">17 лет</span> на рынке IT</div>
+        <div class="image-block__image"><img src="./assets/img/0main/ill4.png" alt="illustratin graph"></div>
+      </div>
+      <div class="image-block">
+        <div class="image-block__image"><img src="./assets/img/0main/ill5.png" alt="illustratin graph"></div>
+        <div class="image-block__text">Широкий кругозор и обширные <span class="image-block__red">знания</span></div>
+      </div>
+      <div class="section__center">
+        <a class="button" href="#">Описание процесса</a>
+      </div>
+    </div>
+  </section>
 </body>
 </html>
