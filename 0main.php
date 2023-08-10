bccb66ce8b2880c56efd30482d6c7d76db8f9589
Kirilll <k.kovalenko@ctrl2go.com>
2023-08-10 17:16:17 +0700

    + main главный экран

--- 0main.php
+++ 0main.php
@@ -6,7 +6,83 @@
 <body>
   <div class="container">
     <div class="container__header"><? include './partials/_header.php'; ?></div>
-    <div class="container__content"></div>
+    <div class="container__content">
+      <section class="section section--main">
+        <div class="wrapper">
+          <h1 class="section__title">Автоматизируем бизнес-процессы с помощью собственных IT-решений и продуктов 1С</h1>
+          <div class="tools">
+            <div class="tools__item js-tool-item">
+              <button class="button button--block js-tool-toggler">Kanban</button>
+              <div class="tools__panel js-tool-panel">
+                <div class="panel panel--no-shadow panel--sm panel--black">
+                  <div class="panel__head">
+                    <button class="panel__close button js-tool-panel-close"><span class="icon icon--cross"></span></button>
+                  </div>
+                  <div class="panel__content">Это система постановки задач и организации рабочих процессов для эффективного достижения поставленных целей, которая предполагает максимальную прозрачность продвижения работ</div>
+                </div>
+              </div>              
+            </div>
+            <div class="tools__item js-tool-item">
+              <button class="button button--block js-tool-toggler button--blue-light">Gherkin</button>
+              <div class="tools__panel js-tool-panel">
+                <div class="panel panel--no-shadow panel--sm panel--black">
+                  <div class="panel__head">
+                    <button class="panel__close button js-tool-panel-close"><span class="icon icon--cross"></span></button>
+                  </div>
+                  <div class="panel__content">Итеративный подход к управлению проектами и разработке программного обеспечения, который помогает командам быстрее и с меньшими проблемами поставлять ценность клиентам</div>
+                </div>
+              </div>
+              
+            </div>
+            <div class="tools__item js-tool-item">
+              <button class="button button--block js-tool-toggler button--blue">CI/CD</button>
+              <div class="tools__panel js-tool-panel">
+                <div class="panel panel--no-shadow panel--sm panel--black">
+                  <div class="panel__head">
+                    <button class="panel__close button js-tool-panel-close"><span class="icon icon--cross"></span></button>
+                  </div>
+                  <div class="panel__content">Набор принципов и практик, которые позволяют разработчикам чаще и надежнее развертывать изменения программного обеспечения</div>
+                </div>
+              </div>
+            </div>
+            <div class="tools__item js-tool-item">
+              <button class="button button--block js-tool-toggler button--red">Git</button>
+              <div class="tools__panel js-tool-panel">
+                <div class="panel panel--no-shadow panel--sm panel--black">
+                  <div class="panel__head">
+                    <button class="panel__close button js-tool-panel-close"><span class="icon icon--cross"></span></button>
+                  </div>
+                  <div class="panel__content">Развитая система контроля версий с активной поддержкой и открытым исходным кодом, которую используют тысячи разработчиков во всем мире</div>
+                </div>
+              </div>
+            </div>
+            <div class="tools__item js-tool-item">
+              <button class="button button--block js-tool-toggler button--yellow">База знаний</button>
+              <div class="tools__panel js-tool-panel">
+                <div class="panel panel--no-shadow panel--sm panel--black">
+                  <div class="panel__head">
+                    <button class="panel__close button js-tool-panel-close"><span class="icon icon--cross"></span></button>
+                  </div>
+                  <div class="panel__content">Онлайн-библиотека, в которой хранится информация о продукте или проекте</div>
+                </div>
+              </div>
+            </div>
+            <div class="tools__item js-tool-item">
+              <button class="button button--block js-tool-toggler">Контроль качества кода</button>
+              <div class="tools__panel js-tool-panel">
+                <div class="panel panel--no-shadow panel--sm panel--black">
+                  <div class="panel__head">
+                    <button class="panel__close button js-tool-panel-close"><span class="icon icon--cross"></span></button>
+                  </div>
+                  <div class="panel__content">Минимизация ошибок перед выпуском программного обеспечения, проверка на наличие недостатков и дефектов с целью минимизации ошибок перед выпуском программного обеспечения</div>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
     <div class="container__footer"><? include './partials/_footer.html'?></div>
   </div>
 </body>
---
+++ dist/js/main.js
@@ -0,0 +1,49 @@
+function setupToolToggler() {
+  const toolTogglers = document.querySelectorAll('.js-tool-toggler');
+
+  toolTogglers.forEach(toggler => {
+    toggler.addEventListener('click', (event) => {
+      const toolItem = toggler.closest('.js-tool-item');
+      const toolPanel = toolItem.querySelector('.js-tool-panel');
+      const isOpen = toolPanel.classList.contains('tools__panel--active');
+      
+      // Закрываем панель, если она уже открыта, иначе открываем
+      if (isOpen) {
+        toolPanel.classList.remove('tools__panel--active');
+      } else {
+        // Закрываем все другие открытые панели перед открытием текущей
+        const activeToolPanels = document.querySelectorAll('.tools__panel--active');
+        activeToolPanels.forEach(panel => {
+          panel.classList.remove('tools__panel--active');
+        });
+        toolPanel.classList.add('tools__panel--active');
+      }
+      
+      event.stopPropagation();
+    });
+  });
+
+  document.addEventListener('click', (event) => {
+    const clickedElement = event.target;
+    const clickedPanelClose = clickedElement.closest('.js-tool-panel-close');
+
+    if (clickedPanelClose) {
+      const toolPanel = clickedPanelClose.closest('.js-tool-panel');
+      toolPanel.classList.remove('tools__panel--active');
+      return;
+    }
+
+    if (!clickedElement.closest('.js-tool-item')) {
+      const activeToolPanels = document.querySelectorAll('.tools__panel--active');
+      activeToolPanels.forEach(panel => {
+        panel.classList.remove('tools__panel--active');
+      });
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  setupToolToggler();
+});
+
+
