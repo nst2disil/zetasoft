186bd3313ba875222a7bc9c1b1cc9afb7e93759b
Kirilll <k.kovalenko@ctrl2go.com>
2024-02-14 13:24:49 +0300

    + favicon

--- partials/_head.php
+++ partials/_head.php
@@ -1,7 +1,9 @@
 <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="shortcut icon" type="image/x-icon" href="./i/favicon.ico" />
+  <link rel=icon type=image/png sizes=32x32 href=/favicon-32x32.png>
+  <link rel=icon type=image/png sizes=16x16 href=/favicon-16x16.png>
+  <link rel=icon type=image/svg+xml sizes=any href=/favicon.svg>
   <meta name="description" content="Продукты компаний ЗетаСофт и 1С - современные решения для автоматизации бизнеса и бизнес-процессов." />
   <meta name="keywords" />
   <meta name="Content-Type" content="text/html; charset=utf-8" />
