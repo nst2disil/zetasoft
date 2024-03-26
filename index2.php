e3bf02545d7dc2974e8726fd3405fff4149c3427
Kirilll <k.kovalenko@ctrl2go.com>
2024-03-26 18:22:32 +0500

    + пустой шаблон движка для подготовки

---
+++ index2.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="ru" xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <title>
+    Автоматизация онлайн-продаж и учёта в 1С | Компания "ЗетаСофт"
+  </title>
+  <link rel="shortcut icon" type="image/x-icon" href="/i/favicon.ico" />
+  <meta name="description"
+    content="Продукты компаний ЗетаСофт и 1С - современные решения для автоматизации бизнеса и бизнес-процессов." />
+  <meta name="keywords" />
+  <meta name="Content-Type" content="text/html; charset=utf-8" />
+  <script>(function (w, d, s, l, i) {
+      w[l] = w[l] || []; w[l].push({
+        'gtm.start':
+          new Date().getTime(), event: 'gtm.js'
+      }); var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+          'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-N5N9N4T');</script>
+  <!-- End Google Tag Manager -->
+
+
+  <!-- styles -->
+
+  <?php $stylesName = 'index'; ?>
+  <?php include './partials/_head.php'; ?>
+  <!-- /styles -->
+
+  <!-- Yandex Webmaster -->
+  <meta name="yandex-verification" content="b72d58cbd74fdb16" />
+
+  <!-- /zetasoft -->
+</head>
+
+<body id="body"><!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N5N9N4T" height="0" width="0"
+      style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+
+  <!-- scripts -->
+
+  <!-- common.js -->
+  <script src="/f/common.js?v=202201131105"></script>
+
+  <!-- /scripts -->
+  <form method="post" action="/" id="form1" novalidate="novalidate">
+    <div class="aspNetHidden">
+      <input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="" />
+    </div>
+
+    <!-- wrapper -->
+    <div id="zw_mp_content" style="margin: 0 auto; width: 100%;">
+
+      <!-- nofooter -->
+      <div class="nofooter">
+        <!-- header -->
+        <header id="ccphupper">
+          
+        </header>
+        <div class="clr"></div>
+        <!-- end of header -->
+        <div id="content-wrapper">
+
+          <section id="ccphworkarea" class="page_full-width">
+
+          </section>
+
+        </div>
+      </div>
+      <!-- end of nofooter -->
+      <!-- footer -->
+      <footer id="ccphbottom" class="footer">
+        <!-- footer-area -->
+        
+        <!-- !!footer-area -->
+      </footer>
+      <!-- end footer -->
+
+    </div>
+    <!-- end of wrapper -->
+  </form>
+  <!-- fancybox.js -->
+  <script src="/f/fancyboxumd.js?v=202207083923"></script>
+  <script src="https://www.google.com/recaptcha/api.js?onload=globalRecaptchaOnLoad&render=explicit" defer
+    async></script>
+</body>
+
+</html>
